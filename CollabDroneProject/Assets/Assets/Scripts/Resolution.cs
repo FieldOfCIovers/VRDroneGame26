7a69e58652437c0a24fe3dd8834d532396bae40f
FieldOfCIovers <begganjack@gmail.com>
2026-04-28 11:26:45 +0100

    Resolution "Fixes"
    - Resolution Fixes
    - Deletion of like 30 Corrupt Metal Textures
    - Build (Might be in the git?)

---
+++ CollabDroneProject/Assets/Assets/Scripts/Resolution.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Screen.SetResolution(1280, 720, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
